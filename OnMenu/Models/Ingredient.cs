82ed1cbbd66199070cafddb4765364bf1bb6260f
TomasCardenal <tomas@pumpun.com>
2019-04-15 13:48:11 +0200

    Completed models for Ingredient and Recipe

--- OnMenu/Models/Ingredient.cs
+++ OnMenu/Models/Ingredient.cs
@@ -1,13 +1,88 @@
 ﻿using System;
 namespace OnMenu.Models
 {
+    /// <summary>
+    /// Model for an Ingredient
+    /// by: Tomás Cardenal López
+    /// </summary>
     public class Ingredient
     {
+        /// <summary>
+        /// The ingredient's name
+        /// </summary>
         string name;
-        float price;
+        /// <summary>
+        /// Gets or sets the ingredient's name.
+        /// </summary>
+        /// <value>The ingredient's name.</value>
+        public string Name { get => name; set => name = value; }
 
-        public Ingredient()
+        /// <summary>
+        /// The ingredient's group
+        /// </summary>
+        string group;
+        /// <summary>
+        /// Gets or sets the food group for this ingredient.
+        /// </summary>
+        /// <value>The food group.</value>
+        public string Group { get => group; set => group = value; }
+
+        /// <summary>
+        /// The ingredient's measuring unit
+        /// </summary>
+        string measure;
+        /// <summary>
+        /// Gets or sets the measure to use with this ingredient.
+        /// </summary>
+        /// <value>The measure.</value>
+        public string Measure { get => measure; set => measure = value; }
+
+        /// <summary>
+        /// Whether this ingredient can be an allergen
+        /// </summary>
+        bool allergen;
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:OnMenu.Models.Ingredient"/> is an allergen.
+        /// </summary>
+        /// <value><c>true</c> if the ingredient is an allergen; otherwise, <c>false</c>.</value>
+        public bool Allergen { get => allergen; set => allergen = value; }
+
+        /// <summary>
+        /// This ingredient's estimated market price
+        /// </summary>
+        float estimatedPrice;
+        /// <summary>
+        /// Gets or sets the estimated price of the ingredient.
+        /// </summary>
+        /// <value>The estimated price of this ingredient.</value>
+        public float EstimatedPrice { get => estimatedPrice; set => estimatedPrice = value; }
+
+        /// <summary>
+        /// The quantity to use of this ingredient.
+        /// </summary>
+        float quantity;
+        /// <summary>
+        /// Gets or sets the quantity of this ingredient.
+        /// </summary>
+        /// <value>The quantity to use, can be zero if the ingredient is to be stored but not used.</value>
+        public float Quantity { get => quantity; set => quantity = value; }
+
+        /// <summary>
+        /// Initializes a new ingredient to it's parameters
+        /// </summary>
+        /// <param name="name">The ingredient's name.</param>
+        /// <param name="group">The ingredient's group.</param>
+        /// <param name="measure">The measuring unit for the ingredient.</param>
+        /// <param name="allergen">If set to <c>true</c>, the ingredient is an allergen.</param>
+        /// <param name="estimatedPrice">Estimated price for the ingredient.</param>
+        public Ingredient(string name, string group, string measure, bool allergen, float estimatedPrice)
         {
+            Name = name;
+            Group = group;
+            Measure = measure;
+            Allergen = allergen;
+            EstimatedPrice = estimatedPrice;
+            Quantity = 0;
         }
     }
 }
--- OnMenu/Models/Recipe.cs
+++ OnMenu/Models/Recipe.cs
@@ -3,14 +3,85 @@ using System.Collections.Generic;
 
 namespace OnMenu.Models
 {
+    /// <summary>
+    /// Model for a recipe
+    /// by: Tomás Cardenal López
+    /// </summary>
     public class Recipe
     {
+        /// <summary>
+        /// The recipe's name
+        /// </summary>
         string name;
-        List<Ingredient> ingredients;
-        string instructions;
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The recipe's name.</value>
+        public string Name { get => name; set => name = value; }
 
-        public Recipe()
+        /// <summary>
+        /// List of ingredients on this recipe
+        /// </summary>
+        List<Ingredient> ingredients;
+        /// <summary>
+        /// Gets or sets the list of ingredients on this recipe
+        /// </summary>
+        /// <value>The list of ingredients.</value>
+        public List<Ingredient> Ingredients { get => ingredients; set => ingredients = value; }
+
+        /// <summary>
+        /// The instructions to follow on this recipe
+        /// </summary>
+        string instructions;
+        /// <summary>
+        /// Gets or sets the instructions.
+        /// </summary>
+        /// <value>The instructions to follow.</value>
+        public string Instructions { get => instructions; set => instructions = value; }
+
+        /// <summary>
+        /// This recipe's rating
+        /// </summary>
+        int rating;
+        /// <summary>
+        /// Gets or sets the rating.
+        /// </summary>
+        /// <value>The rating, between 0 and 5, both included.</value>
+        public int Rating
         {
+            get => rating;
+            set
+            {
+                if (value < 0)
+                {
+                    rating = 0;
+                }
+                else if (value > 5)
+                {
+                    rating = 5;
+                }
+                else
+                {
+                    rating = value;
+                }
+            }
         }
+
+
+        /// <summary>
+        /// Initializes a new recipe to it's parameters/>
+        /// </summary>
+        /// <param name="name">The recipe's name.</param>
+        /// <param name="instructions">The instructions to follow on this recipe.</param>
+        /// <param name="ingredients">List of ingredients.</param>
+        /// <param name="rating">The recipe's name.</param>
+        public Recipe(string name, string instructions, List<Ingredient> ingredients, int rating)
+        {
+            Name = name;
+            Instructions = instructions;
+            Ingredients = ingredients;
+            Rating = rating;
+        }
+
     }
 }
